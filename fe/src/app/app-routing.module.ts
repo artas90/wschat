71747ce4bed7453e1621a4c73c58302703b66b8c
Andrey Sokolov <artas90@gmail.com>
2021-02-06 02:11:28 +0200

    change pages order

--- fe/src/app/app-routing.module.ts
+++ fe/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '',   redirectTo: '/about', pathMatch: 'full' }
+  { path: '',   redirectTo: '/params', pathMatch: 'full' }
 ];
 
 @NgModule({
